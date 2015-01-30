57e26fe9245ce498dcba370518a8a242a921fefc
minaribanchan <minaribanchan@gmail.com>
2015-01-30 01:07:16 -0800

    ADD : test console program

--- sshpgt.py
+++ sshpgt.py
@@ -8,16 +8,68 @@ import sshpt
 
 
 class Console(QtGui.QPlainTextEdit):
+    __pyqtSignals__ = ("pythonOutput(const QString &)",)
+
     def __init__(self, parent=None):
         super(Console, self).__init__(parent)
 
-        localEchoEnable=False
+        self.history = []
+        self.current = -1
 
         self.document().setMaximumBlockCount(100)
         p = QtGui.QPalette()
         p.setColor(QtGui.QPalette.Base, QtCore.Qt.black)
         p.setColor(QtGui.QPalette.Text, QtCore.Qt.green)
         self.setPalette(p)
+
+        self.connect(self, QtCore.SIGNAL("returnPressed()"), self.execute)
+
+    def putData(self, data):
+        """ InsertData """
+        self.insertPlainText(data)
+
+        bar = self.verticalScrollBar()
+        bar.setValue(bar.maximum())
+
+    def keyReleaseEvent(self, event):
+        if event.type() == QtCore.QEvent.KeyRelease:
+            if event.key() == QtCore.Qt.Key_Up:
+                current = max(0, self.current - 1)
+                if 0 <= current < len(self.history):
+                    self.setText(self.history[current])
+                    self.current = current
+                event.accept()
+            elif event.key() == QtCore.Qt.Key_Down:
+                current = min(len(self.history), self.current + 1)
+                if 0 <= current < len(self.history):
+                    self.setText(self.history[current])
+                else:
+                    self.clear()
+                self.current = current
+
+                event.accept()
+    def mousePressEvent(self, e):
+        self.setFocus()
+
+    def execute(self):
+        qApp = QtGui.qApp
+
+        self.expression = self.text()
+        try:
+            result = str(eval(unicode(self.expression)))
+
+            self.emit(QtCore.SIGNAL("pythonOutput(const QString &)"),
+                    QtCore.QString(result))
+
+            self.clear()
+            self.history.append(self.expression)
+            self.current = len(self.history)
+        except:
+            pass
+
+
+
+
 
 
 class Window(QtGui.QMainWindow):
