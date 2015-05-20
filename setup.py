597a892d13e6fe09a7b30bcf3f996f46303a0693
haginara <minaribanchan@gmail.com>
2015-05-20 16:15:02 -0700

    REL: 1.3.3 MOD: Changed the setup.py and information

--- setup.py
+++ setup.py
@@ -16,22 +16,20 @@ setup(
     license = sshpt.__license__,
     version = version,
     author = "{},{}".format(sshpt.__author__, sshpt.__second_author__),
-    author_email = 'YouKnowWho@YouKnowWhat.com',
-    url = 'http://code.google.com/p/sshpt/',
+    author_email = 'haginara@gmail.com',
+    url = 'https://github.com/haginara/sshpt'
     description = 'SSH Power Tool - Run commands and copy files to multiple servers simultaneously WITHOUT requiring pre-shared authentication keys',
     long_description = open('README.txt').read(),
     keywords='ssh administration parallel',
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: Unix",
         "Environment :: Console",
-        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.7",
         "Topic :: System :: Systems Administration",
     ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     packages = find_packages(exclude=EXCLUDE_FROM_PACKAGES),
-    #package_dir =  {'sshpt': 'sshpt'},
-    #scripts = ['sshpt/sshpt.py'],
     entry_points = {
         'console_scripts': [
             'sshpt = sshpt:main',
