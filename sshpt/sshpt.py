2d52c0f0cf3f3ff23711974c9daa5a65fa50df1b
haginara <minaribanchan@gmail.com>
2015-05-21 00:41:43 -0700

    Add Copyright

--- sshpt/sshpt.py
+++ sshpt/sshpt.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#       sshpt.py
-#
 #       Copyright 2011 Dan McDougall <YouKnowWho@YouKnowWhat.com>
+#       Copyright 2015 Jonghak Choi <haginara@gmail.com>
 #
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
