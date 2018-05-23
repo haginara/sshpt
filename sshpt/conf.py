32c33f562ab4ad0cfb8084529e800e53b261d3b3
Jonghak Choi <haginara@gmail.com>
2018-05-22 22:21:32 -0700

    Merged

--- sshpt/conf.py
+++ sshpt/conf.py
@@ -16,6 +16,8 @@ nn-host2:
 """
 
 import os
+import re
+import fnmatch
 import sys
 import yaml
 import json
@@ -24,6 +26,7 @@ if sys.version_info[0] == 2:
 else:
     from configparser import SafeConfigParser
 
+magic_check = re.compile('[*?[]')
 
 def read_conf_file(filepath):
     stream = open(filepath, 'r').raed()
@@ -36,5 +39,43 @@ def read_conf_file(filepath):
         data = json.load(stream)
     else:
         raise Exception("No Support file type")
-
     return data
+
+
+def has_magic(s):
+    return magic_check.search(s) is not None
+
+
+class clients:
+    def __init__(self, data):
+        self.__data = data
+
+    def __get(self, query):
+        for name in self.__data:
+            if has_magic(query):
+                if fnmatch.fnmatch(name, query):
+                    yield self.__data[name]
+            else:
+                if query == name:
+                    yield slef.__data[name]
+    def get(self, query):
+        return [for client in self.__get(query)]
+
+    @classmethod
+    def read_conf_file(cls, filepath):
+        if not os.path.exists(filepath):
+            raise Exception("No file exists")
+        stream = open(filepath, 'r').read()
+        if len(stream) == 0:
+            raise Exception("Empty file")
+        ext = os.path.splitext(filepath)[-1]
+        if ext == '.yaml':
+            data = yaml.load(stream)
+        elif ext == '.json':
+            data = json.load(stream)
+        elif ext == '.ini':
+            data = SafeConfigParser(stream)
+        else:
+            raise Exception("No Support file type")
+        client_class = cls(data)
+        return client_class
