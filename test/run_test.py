c12a848a7c2de3e908274300c28116ed6f669330
Jonghak <haginara@gmail.com>
2020-09-27 22:19:24 -0700

    Update mock test

--- test/run_test.py
+++ test/run_test.py
@@ -3,13 +3,24 @@ from __future__ import print_function
 import os
 import sys
 import logging
+import unittest
+from unittest import mock
+import pytest
+import argparse
+from subprocess import Popen, PIPE
+
 from os.path import dirname
 from os.path import abspath
 root_path = dirname(dirname(abspath(__file__)))
 sys.path.append(root_path)
 
-logging.basicConfig(level=logging.DEBUG)
+from sshpt import version
+from sshpt import main
 
-if __name__ == '__main__':
-    from sshpt import main
-    main.main()
+def test_version(capsys):
+    with mock.patch('sys.argv', ['sshpt', '-v']):
+        with pytest.raises(SystemExit) as exc:
+            ret = main.main()
+        out, err = capsys.readouterr()
+        assert(out.strip() == version.__version__)
+        assert(exc.value.code == 0)
